07aeace85630caf1f4f185205648cc2cba5774c5
KyCodes <kyler@ky.codes>
2022-03-30 20:42:05 -0600

    fixes: pseudo classes to handle overflow negated margin

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,10 +5,10 @@ import * as styles from '../styles/Main.module.css'
 export default function Main() {
     return (
         <div className={styles.container}>
-            <div className={styles.card}>
+            <a href='mailto&#58;ky%6Cer&#64;k%7&#57;&#46;c%&#54;F%64e&#115;' className={styles.card}>
                 <h1>Inquire about KyCodes custom app and web development so your next project is here!</h1>
                 <p>Make more!</p>
-            </div>
+            </a>
             <a href='https://ideality.dev/' className={styles.card}>
                 <StaticImage placeholder='blurred' imgStyle={{ borderRadius: '8px' }} src='../images/ideality-banner.webp' alt="Ideality App Banner" />
                 <p>Ideality</p>
